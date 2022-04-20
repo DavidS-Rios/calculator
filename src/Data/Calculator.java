44c22d4c94e30f5951a040caffe69af3457c9fcb
David Stevens Rios Gonzalez <davidstevens.rios@globant.com>
2022-04-20 10:12:47 -0500

    multiply and divide added

--- src/Data/Calculator.java
+++ src/Data/Calculator.java
@@ -7,5 +7,12 @@ public class Calculator {
 
     public static float subtract(float num1, float num2){
         return num1-num2;
+
+    public static float multiply(float num1, float num2){
+        return num1*num2;
+    }
+
+    public static float divide(float num1, float num2){
+        return num1/num2;
     }
 }
