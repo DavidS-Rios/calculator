8d87c603d92479c2368511f3cc8e8f79432c85c3
David Stevens Rios Gonzalez <davidstevens.rios@globant.com>
2022-04-20 09:46:05 -0500

    sum and subtract added

---
+++ src/Data/Calculator.java
@@ -0,0 +1,11 @@
+package Data;
+
+public class Calculator {
+    public static float sum(float num1, float num2){
+        return num1+num2;
+    }
+
+    public static float subtract(float num1, float num2){
+        return num1-num2;
+    }
+}
